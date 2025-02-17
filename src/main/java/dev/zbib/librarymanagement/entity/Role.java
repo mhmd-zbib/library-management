99c745e50f42160b7f521017cd7fb0fb71a6208a
mhmd-zbib <mohammad.h.zbib@gmail.com>
2025-02-17 20:47:57 +0200

    feat: setup user and role entity

---
+++ src/main/java/dev/zbib/librarymanagement/entity/Role.java
@@ -0,0 +1,24 @@
+package dev.zbib.librarymanagement.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "roles")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private UUID id;
+
+    @Column(unique = true)
+    private String name;
+
+}
---
+++ src/main/java/dev/zbib/librarymanagement/entity/User.java
@@ -0,0 +1,30 @@
+package dev.zbib.librarymanagement.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private UUID id;
+
+    @Column(unique = true)
+    private String email;
+
+    private String password;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "role_id")
+    private Role role;
+
+}
