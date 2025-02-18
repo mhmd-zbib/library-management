d246581a45f806ebc2132360342754c616feaa34
mhmd-zbib <mohammad.h.zbib@gmail.com>
2025-02-18 07:11:11 +0200

    feat: add auth dto

---
+++ src/main/java/dev/zbib/librarymanagement/dto/RegisterRequest.java
@@ -0,0 +1,22 @@
+package dev.zbib.librarymanagement.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class RegisterRequest {
+
+    @NotBlank(message = "Email is required")
+    @Email(message = "Invalid email format")
+    private String email;
+
+    @NotBlank(message = "Password is required")
+    @Pattern(regexp = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=])(?=\\S+$).{8,}$",
+            message = "Password must be at least 8 characters long and contain at least one digit, one uppercase letter, one lowercase letter, and one special character")
+    private String password;
+
+}
