04d6dccb54a7e5b57b1955cf603e16275dd1dede
mhmd-zbib <mohammad.h.zbib@gmail.com>
2025-02-19 10:55:38 +0200

    fix: docker setup

--- src/main/java/dev/zbib/librarymanagement/dto/BorrowingRecordFilter.java
+++ src/main/java/dev/zbib/librarymanagement/dto/BorrowingRecordFilter.java
@@ -3,12 +3,15 @@ package dev.zbib.librarymanagement.dto;
 import dev.zbib.librarymanagement.entity.BorrowingStatus;
 import lombok.Builder;
 import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.format.annotation.DateTimeFormat;
 
 import java.time.LocalDateTime;
 import java.util.UUID;
 
-@Data
+@Getter
+@Setter
 @Builder
 public class BorrowingRecordFilter {
     private UUID patronId;
